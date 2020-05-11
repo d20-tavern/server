237f5a526884e5d4c646b6c64ba0af729474ca08
Tauntalus <croomb@spu.edu>
2020-05-11 14:24:05 -0700

    fixed compiler errors relating to organization and restructuring

--- tavern_pathfinder/src/lib.rs
+++ tavern_pathfinder/src/lib.rs
@@ -54,7 +54,7 @@ pub enum Attribute {
 }
 
 #[derive(Serialize,Deserialize)]
-pub enum Skill 
+pub enum Skill {
 	Acrobatics, 
 	Appraise, 
 	Bluff, 
@@ -96,7 +96,7 @@ pub enum SaveThrow {
 	Fortitude,
 	Reflex,
 	Will,
-	
+}	
 
 #[derive(Serialize,Deserialize)]
 pub enum CharacterStat {
@@ -228,8 +228,8 @@ pub enum DamageType {
 	Positive, 
 	Negative, 
 	Nonlethal,
-	}
 }
+
 
 pub mod character {
 	use serde::{Serialize,Deserialize};
@@ -238,17 +238,17 @@ pub mod character {
 	#[derive(Serialize,Deserialize)]
 	pub struct Character {
 		char_id: Uuid,
-		race_id: Race,
-		//deity_id: deity,
+		race_: Race,
+		deity: crate::religion::Deity,
 
 		name: String,
 		age: u32,
-		gender: Gender,
-		alignment::Alignment,
+		gender: crate::Gender,
+		alignment: crate::Alignment,
 		backstory: String,
 		height: u32,
 		weight: u32,
-		size: Size,
+		size: crate::Size,
 
 		strength: u32,
 		dexterity: u32,
@@ -267,17 +267,18 @@ pub mod character {
 	}
 
 	#[derive(Serialize,Deserialize)]
-	pub struct race {
+	pub struct Race {
 		race_id: Uuid,
 		type_id: RaceType,
 		subtype_id: RaceSubtype,
 
 		name: String,
 		move_speed: u32,
-		size: Size,
+		size: crate::Size,
 		languages: Vec<String>,
 	}
 
+	#[derive(Serialize,Deserialize)]
 	pub struct RaceType {
 		type_id: Uuid,
 
@@ -286,212 +287,216 @@ pub mod character {
 		bab_per_hit_die: f32,
 	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct RaceSubtype {
-        subtype_id: Uuid,
-        name: String,
-    }
+	#[derive(Serialize,Deserialize)]
+	pub struct RaceSubtype {
+		subtype_id: Uuid,
+		name: String,
+	}
 }
 
 pub mod class {
 	use serde::{Serialize,Deserialize};
-    use uuid::Uuid;
+	use uuid::Uuid;
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Class {
-        class_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Class {
+		class_id: Uuid,
 
-        name: String,
-        hit_die: String,
-        starting_wealth: String,
-        bab_per_level: f32,
-        skills_per_level: i32,
-        skills_attr: Attribute,
-    }
+		name: String,
+		hit_die: String,
+		starting_wealth: String,
+		bab_per_level: f32,
+		skills_per_level: i32,
+		skills_attr: crate::Attribute,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Subclass {
-        subclass_id: Uuid,
-        class: Class,
+	#[derive(Serialize,Deserialize)]
+	pub struct Subclass {
+		subclass_id: Uuid,
+		class: Class,
 
-        caster_type: CasterType,
-        casting_attr: Attribute,
-    }
+		caster_type: crate::CasterType,
+		casting_attr: crate::Attribute,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Feature {
-        feature_id: Uuid,
-        description: String,
-    }
-}
+	#[derive(Serialize,Deserialize)]
+	pub struct Feature {
+		feature_id: Uuid,
+		description: String,
+	}
+
 	pub mod proficiencies {
 		use serde::{Serialize,Deserialize};	
 		use uuid::Uuid;
-
+		#[derive(Serialize,Deserialize)]
 		pub struct ClassWeaponsNotProficient {
 			class_id: Uuid,
-			weapon_classes: Vec<WeaponClass>
+			weapon_classes: Vec<crate::WeaponClass>
 		}
 
+		#[derive(Serialize,Deserialize)]
 		pub struct ClassArmorNotProficient {
 			class_id: Uuid,
-			armor_classes: Vec<ArmorClass>
+			armor_classes: Vec<crate::ArmorClass>
 		}
 	}
 }
 
 pub mod feat {
-    use serde::{Serialize,Deserialize};   
-    use uuid::Uuid;
+	use serde::{Serialize,Deserialize};   
+	use uuid::Uuid;
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Feat {
-        feat_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Feat {
+		feat_id: Uuid,
 
-        short_description: String,
-        long_description: String,
-    }
+		short_description: String,
+		long_description: String,
+	}
 
-    pub struct SkillFeatUnit {
-        skill_unit_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct SkillFeatUnit {
+		skill_unit_id: Uuid,
 
-        req_skil: Skill,
-        ranks: u8,
-    }
+		req_skil: crate::Skill,
+		ranks: u8,
+	}
 
-    pub struct AttributeFeatUnit {
-        attr_unit_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct AttributeFeatUnit {
+		attr_unit_id: Uuid,
 
-        req_attr: Attribute,
-        score: u8
-    }
+		req_attr: crate::Attribute,
+		score: u8
+	}
 
-    pub struct RequiredFeat {
-        feat_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct RequiredFeat {
+		feat_id: Uuid,
 
-        required_feat: Feat
-    }
-}
+		required_feat: Feat,
+	}
 }
 
 
 pub mod spell {
-    use serde::{Serialize,Deserialize}; 
-    use uuid::Uuid;
+	use serde::{Serialize,Deserialize}; 
+	use uuid::Uuid;
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Spell {
-        spell_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Spell {
+		spell_id: Uuid,
 
-        name: String,
-        level: u32,
-        school: MagicSchool,
+		name: String,
+		level: u32,
+		school: crate::MagicSchool,
 
-        casting_time: u32,
-        range: SpellRange,
-        area: String,
-        duration_per_level: u32,
-        saving_throw: SaveThrow,
-        spell_resistance: bool,
-        description: String,
-    }
+		casting_time: u32,
+		range: crate::SpellRange,
+		area: String,
+		duration_per_level: u32,
+		saving_throw: crate::SaveThrow,
+		spell_resistance: bool,
+		description: String,
+	}
 
-    pub struct SpellComponent {
-        spell: Spell
-    }
+	#[derive(Serialize,Deserialize)]
+	pub struct SpellComponent {
+		spell: Spell,
+	}
 }
 
 pub mod religion {
-    use serde::{Serialize,Deserialize};    
-    use uuid::Uuid;
+	use serde::{Serialize,Deserialize};	
+	use uuid::Uuid;
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Deity {
-        deity_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Deity {
+		deity_id: Uuid,
 
-        name: String,
-        description: String,
-        favored_animals: Vec<String>,
-    }
+		name: String,
+		description: String,
+		favored_animals: Vec<String>,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Domain {
-        domain_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Domain {
+		domain_id: Uuid,
 
-        name: String,
-        description: String,
-        power_description: String,
-    }
+		name: String,
+		description: String,
+		power_description: String,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Subdomain {
-        domain_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Subdomain {
+		domain_id: Uuid,
 
-        name: String,
-        description: String,
-    }
+		name: String,
+		description: String,
+	}
 }
 
 pub mod item {
-    use serde::{Serialize,Deserialize};
-    use crate::character;
-    use uuid::Uuid;
+	use serde::{Serialize,Deserialize};
+	use crate::character;
+	use uuid::Uuid;
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Item {
-        item_id: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Item {
+		item_id: Uuid,
 
-        name: String,
-        description: String,
-        cost: u32,
-        weight: u32,
-        equip_slot: EquipmentSlot,
-    }
+		name: String,
+		description: String,
+		cost: u32,
+		weight: u32,
+		equip_slot: crate::EquipmentSlot,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Bag {
-        bag_id: Uuid,
-        char: character::Character,
-        item: Item,
+	#[derive(Serialize,Deserialize)]
+	pub struct Bag {
+		bag_id: Uuid,
+		character: character::Character,
+		item: Item,
 
-        capacity: u32,
-    }
+		capacity: u32,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Weapon {
-        item: Item,
-        material: Material,
+	#[derive(Serialize,Deserialize)]
+	pub struct Weapon {
+		item: Item,
+		material: Material,
 
-        weapon_range: Range<u32>,
-        crit_range: Range<u32>,
-        damage: Vec<String>,
-        damage_type: Vec<DamageType>,
-        weapon_type: WeaponClass,
-    }
+		weapon_range: std::ops::Range<u32>,
+		crit_range: std::ops::Range<u32>,
+		damage: Vec<String>,
+		damage_type: Vec<crate::DamageType>,
+		weapon_type: crate::WeaponClass,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Armor {
-        item: Item,
-        material: Material,
+	#[derive(Serialize,Deserialize)]
+	pub struct Armor {
+		item: Item,
+		material: Material,
 
-        max_dex_bonus: u32,
-        ac: u32,
-        spell_failure: u32,
-        check_penalty: u32,
-        armor_type: ArmorClass,
-    }
+		max_dex_bonus: u32,
+		ac: u32,
+		spell_failure: u32,
+		check_penalty: u32,
+		armor_type: crate::ArmorClass,
+	}
 
-    #[derive(Serialize,Deserialize)]
-    pub struct Material {
-        material: Uuid,
+	#[derive(Serialize,Deserialize)]
+	pub struct Material {
+		material: Uuid,
 
-        name: String,
-        description: String,
+		name: String,
+		description: String,
 
-        hp_per_inch: u32,
-        hardness: u32,
-        
-    }
+		hp_per_inch: u32,
+		hardness: u32,
+		
+	}
 }
 
 pub mod effects {
@@ -506,43 +511,42 @@ pub mod effects {
 		long_description: String,
 	}
 
-    #[derive(Serialize, Deserialze)]
-    pub struct AttributeUnit {
-        attr_unit_id: Uuid,
+	#[derive(Serialize, Deserialize)]
+	pub struct AttributeUnit {
+		attr_unit_id: Uuid,
 
-        base_attr: Attribute,
-        modifier: i32,
-    }
+		base_attr: crate::Attribute,
+		modifier: i32,
+	}
 
-    #[derive(Serialize, Deserialze)]
-    pub struct SkillUnit {
-        skill_unit_id: Uuid,
+	#[derive(Serialize, Deserialize)]
+	pub struct SkillUnit {
+		skill_unit_id: Uuid,
 
-        skill: Skill,
-        modifier: i32,
-    }
+		skill: crate::Skill,
+		modifier: i32,
+	}
 
-    #[derive(Serialize, Deserialze)]
-    pub struct CharacterUnit {
-        char_unit_id: Uuid,
+	#[derive(Serialize, Deserialize)]
+	pub struct CharacterUnit {
+		char_unit_id: Uuid,
 
-        character_stat: CharacterStat,
-        modifier: i32
-    }
+		character_stat: crate::CharacterStat,
+		modifier: i32
+	}
 
-    #[derive(Serialize, Deserialze)]
-    pub struct CombatUnit {
-        combat_unit_id: Uuid,
+	#[derive(Serialize, Deserialize)]
+	pub struct CombatUnit {
+		combat_unit_id: Uuid,
 
-        combat_stat: CombatStat,
-        modifier: i32,
-    }
+		combat_stat: crate::CombatStat,
+		modifier: i32,
+	}
 
-    #[derive(Serialize, Deserialze)]
-    pub struct MiscUnit {
-        misc_unit_id: Uuid,
+	#[derive(Serialize, Deserialize)]
+	pub struct MiscUnit {
+		misc_unit_id: Uuid,
 
-        description: String,
-    }
-}
+		description: String,
+	}
 }
