e2de1157844f2c652bcf38f12119e674c6cf8f52
Michael Bryant <shadow53@shadow53.com>
2020-05-14 21:39:38 -0700

    tavern_server: add auth to server init
    Creates one large warp filter that can be passed to `warp::serve`. Adds
    the login and registration paths to this.

--- tavern_server/src/lib.rs
+++ tavern_server/src/lib.rs
@@ -1,5 +1,22 @@
+use warp::{Filter, Reply};
+use warp::filters::BoxedFilter;
+
 pub mod auth;
 pub mod config;
 pub mod db;
 pub mod forms;
 pub mod status;
+
+pub fn setup_server() -> BoxedFilter<(impl Reply,)> {
+    let login = warp::get()
+                    .and(warp::path("login"))
+                    .and(auth::login_filter());
+    let register = warp::post()
+                    .and(warp::path("register"))
+                    .and(auth::register_filter());
+
+    warp::any().and(
+        login
+        .or(register)
+    ).boxed()
+}
