0ff9aa184627a020d097f41c2f4e680dc9b0f890
Tauntalus <croomb@spu.edu>
2020-05-17 22:33:00 -0700

    minor update to use proficiencies

--- tavern_pathfinder/src/class/mod.rs
+++ tavern_pathfinder/src/class/mod.rs
@@ -1,30 +1,35 @@
 use serde::{Serialize,Deserialize};
 use uuid::Uuid;
 
+use crate::Attribute;
+use crate::CasterType;
+use proficiencies;
+
 #[derive(Serialize,Deserialize)]
 pub struct Class {
-    class_id: Uuid,
+    id: Uuid,
+    subclasses: Subclass[],
 
     name: String,
     hit_die: String,
     starting_wealth: String,
     bab_per_level: f32,
     skills_per_level: i32,
-    skills_attr: crate::Attribute,
+    skills_attr: Attribute,
 }
 
 #[derive(Serialize,Deserialize)]
 pub struct Subclass {
-    subclass_id: Uuid,
-    class: Class,
+    id: Uuid,
+    parent_class: Class,
 
-    caster_type: crate::CasterType,
-    casting_attr: crate::Attribute,
+    caster_type: CasterType,
+    casting_attr: Attribute,
 }
 
 #[derive(Serialize,Deserialize)]
 pub struct Feature {
-    feature_id: Uuid,
+    id: Uuid,
     description: String,
 }
 
